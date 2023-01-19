39ee5a87e8ad98db03c1e828e850e5e8ee2cccdd
P-rin <actprin@gmail.com>
2023-01-19 21:51:01 +0700

    work3

---
+++ work3/word-unique-count/target/debug/deps/word_unique_count.d
@@ -0,0 +1,5 @@
+C:\Users\ADMIN\Desktop\w_rust\work3\word-unique-count\target\debug\deps\word_unique_count.exe: src\main.rs
+
+C:\Users\ADMIN\Desktop\w_rust\work3\word-unique-count\target\debug\deps\word_unique_count.d: src\main.rs
+
+src\main.rs:
---
+++ work3/word-unique-count/target/debug/word-unique-count.d
@@ -0,0 +1,1 @@
+C:\Users\ADMIN\Desktop\w_rust\work3\word-unique-count\target\debug\word-unique-count.exe: C:\Users\ADMIN\Desktop\w_rust\work3\word-unique-count\src\main.rs
